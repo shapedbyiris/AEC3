ce845c4adab4347b0375612c5fb8d375a774487f
Ariel Elkin <ariel@shapedbyiris.com>
2022-05-23 14:12:01 +0200

    updates

--- make-xcframeworks.sh
+++ make-xcframeworks.sh
@@ -1,32 +1,34 @@
-# DEVELOPERID_CERTIFICATE_NAME="Developer ID Application: TGR1.618 limited (5RMZ3ZU3DS)"
-# codesign --sign "${DEVELOPERID_CERTIFICATE_NAME}" static-libs/libapi.a --force --deep  --timestamp --options runtime
-# codesign --sign "${DEVELOPERID_CERTIFICATE_NAME}" static-libs/libAEC3.a --force --deep  --timestamp --options runtime
-# codesign --sign "${DEVELOPERID_CERTIFICATE_NAME}" static-libs/libbase.a --force --deep  --timestamp --options runtime
-
 rm -rf macos-products/headers || true
 rm -rf macos-products/base.xcframework || true
 rm -rf macos-products/api.xcframework || true
 rm -rf macos-products/AEC3.xcframework || true
 
+sh generate-macos-project.sh
+
+DEVELOPERID_CERTIFICATE_NAME="Developer ID Application: TGR1.618 limited (5RMZ3ZU3DS)"
+xcodebuild build \
+    -project macOS/AEC3.xcodeproj \
+    -target api \
+    -sdk macosx \
+    CODE_SIGN_IDENTITY=${DEVELOPERID_CERTIFICATE_NAME} \
+    | xcpretty
+
 mkdir ./macos-products/headers
 rsync -avhm --include='api' --include='*.h' --exclude='*' ./ ./macos-products/headers/api/
 rsync -avhm --include='*/' --include='*.h' --exclude='*' ./base/rtc_base ./macos-products/headers/api/
 rsync -avhm --include='*/' --include='*.h' --exclude='*' ./base/abseil/absl ./macos-products/headers/api/
-
 rsync -avhm --include='*/' --include='*.h' --exclude='*' ./audio_processing ./macos-products/headers/audio_processing
 
 xcodebuild -create-xcframework \
-    -library static-libs/libapi.a \
+    -library output/Darwin/Debug_x64/libapi.a \
     -headers macos-products/headers/api \
     -output macos-products/api.xcframework
 
 xcodebuild -create-xcframework \
-    -library static-libs/libbase.a \
+    -library output/Darwin/Debug_x64/libbase.a \
     -output macos-products/base.xcframework
 
 xcodebuild -create-xcframework \
-    -library static-libs/libAEC3.a \
+    -library output/Darwin/Debug_x64/libAEC3.a \
     -headers macos-products/headers/audio_processing \
     -output macos-products/AEC3.xcframework
-
-
