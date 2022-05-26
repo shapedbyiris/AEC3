9c0b383f9b2864ac42499c1e818307d2b960af77
Ariel Elkin <ariel@shapedbyiris.com>
2022-05-26 17:25:24 +0200

    add optimised frameworks

--- make-xcframeworks.sh
+++ make-xcframeworks.sh
@@ -1,14 +1,27 @@
-rm -rf macos-products || true
+set -xeuo pipefail
+rm -rf macos-products output || true
+rm -rf macOS/build UninstalledProducts || true
 
 sh generate-macos-project.sh
+
+
+    # SKIP_INSTALL=NO \
+    # BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+    # CODE_SIGN_STYLE=Manual \
+    # CODE_SIGN_IDENTITY=${DEVELOPERID_CERTIFICATE_NAME} \
+    # DEVELOPMENT_TEAM=5RMZ3ZU3DS \
+
 
 DEVELOPERID_CERTIFICATE_NAME="Developer ID Application: TGR1.618 limited (5RMZ3ZU3DS)"
 xcodebuild build \
     -project macOS/AEC3.xcodeproj \
     -target api \
     -sdk macosx \
-    CODE_SIGN_IDENTITY=${DEVELOPERID_CERTIFICATE_NAME} \
+    -destination "generic/platform=macOS" \
+    GCC_OPTIMIZATION_LEVEL=3 \
+    OTHER_CPLUSPLUSFLAGS="-DNDEBUG '-std=gnu++14'" \
     | xcpretty
+    
 
 mkdir -p ./macos-products/headers
 rsync -avhm --include='api' --include='*.h' --exclude='*' ./ ./macos-products/headers/api/
