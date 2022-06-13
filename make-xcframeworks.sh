acc1615b5ccbb737bf09164cde369281b170064e
Ariel Elkin <ariel@shapedbyiris.com>
2022-06-13 15:45:41 +0200

    add files to reproduce issue

--- make-xcframeworks.sh
+++ make-xcframeworks.sh
@@ -4,24 +4,17 @@ rm -rf macOS/build UninstalledProducts || true
 
 sh generate-macos-project.sh
 
+# Make debug versions:
 
-    # SKIP_INSTALL=NO \
-    # BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
-    # CODE_SIGN_STYLE=Manual \
-    # CODE_SIGN_IDENTITY=${DEVELOPERID_CERTIFICATE_NAME} \
-    # DEVELOPMENT_TEAM=5RMZ3ZU3DS \
-
-
-DEVELOPERID_CERTIFICATE_NAME="Developer ID Application: TGR1.618 limited (5RMZ3ZU3DS)"
 xcodebuild build \
     -project macOS/AEC3.xcodeproj \
     -target api \
     -sdk macosx \
+    -configuration Debug \
     -destination "generic/platform=macOS" \
     GCC_OPTIMIZATION_LEVEL=3 \
     OTHER_CPLUSPLUSFLAGS="-DNDEBUG '-std=gnu++14'" \
     | xcpretty
-    
 
 mkdir -p ./macos-products/headers
 rsync -avhm --include='api' --include='*.h' --exclude='*' ./ ./macos-products/headers/api/
@@ -40,5 +33,33 @@ xcodebuild -create-xcframework \
 
 xcodebuild -create-xcframework \
     -library output/Darwin/Debug_x64/libAEC3.a \
+    -headers macos-products/headers/audio_processing \
+    -output macos-products/AEC3.xcframework
+
+
+# Make release versions:
+
+xcodebuild build \
+    -project macOS/AEC3.xcodeproj \
+    -target api \
+    -sdk macosx \
+    -configuration Release \
+    -destination "generic/platform=macOS" \
+    GCC_OPTIMIZATION_LEVEL=3 \
+    OTHER_CPLUSPLUSFLAGS="-DNDEBUG '-std=gnu++14'" \
+    | xcpretty
+
+
+xcodebuild -create-xcframework \
+    -library output/Darwin/Release_x64/libapi.a \
+    -headers macos-products/headers/api \
+    -output macos-products/api.xcframework
+
+xcodebuild -create-xcframework \
+    -library output/Darwin/Release_x64/libbase.a \
+    -output macos-products/base.xcframework
+
+xcodebuild -create-xcframework \
+    -library output/Darwin/Release_x64/libAEC3.a \
     -headers macos-products/headers/audio_processing \
     -output macos-products/AEC3.xcframework
