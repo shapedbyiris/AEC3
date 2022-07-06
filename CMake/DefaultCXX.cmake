3e140ba62fcdae213334f82f873c8fa32af271f8
Sergey Podobry <sergius@apriorit.com>
2022-07-06 17:24:54 +0300

    Add nuspec, change C++ runtime library to static

--- CMake/DefaultCXX.cmake
+++ CMake/DefaultCXX.cmake
@@ -6,7 +6,7 @@ if(MSVC)
     create_property_reader("DEFAULT_CXX_EXCEPTION_HANDLING")
     create_property_reader("DEFAULT_CXX_DEBUG_INFORMATION_FORMAT")
 
-    set_target_properties("${PROPS_TARGET}" PROPERTIES MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
+    set_target_properties("${PROPS_TARGET}" PROPERTIES MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
     set_config_specific_property("DEFAULT_CXX_EXCEPTION_HANDLING" "/EHsc")
     set_config_specific_property("DEFAULT_CXX_DEBUG_INFORMATION_FORMAT" "/Zi")
 endif()
