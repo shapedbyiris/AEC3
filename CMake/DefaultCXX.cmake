7f066da9c0bd20194b8c91803494e3c33e615ad0
Sergey Podobry <sergius@apriorit.com>
2022-07-06 19:09:04 +0300

    Enable multiprocessor build

--- CMake/DefaultCXX.cmake
+++ CMake/DefaultCXX.cmake
@@ -7,6 +7,7 @@ if(MSVC)
     create_property_reader("DEFAULT_CXX_DEBUG_INFORMATION_FORMAT")
 
     set_target_properties("${PROPS_TARGET}" PROPERTIES MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+    target_compile_options("${PROPS_TARGET}" PRIVATE /MP)
     set_config_specific_property("DEFAULT_CXX_EXCEPTION_HANDLING" "/EHsc")
     set_config_specific_property("DEFAULT_CXX_DEBUG_INFORMATION_FORMAT" "/Zi")
 endif()
